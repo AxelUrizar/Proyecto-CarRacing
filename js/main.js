4c9bf9e93bf1828afb172940778fe9a26147ab8b
Rafael3994 <rafael3994@gmail.com>
2022-01-07 18:02:44 +0100

    implementacion musica

--- js/main.js
+++ js/main.js
@@ -1,22 +1,72 @@
 // ------------------------- Poner musica al juego ----------------------------------- //
 
-//let music = new Audio('/music/MainMenu.mp3');
-//music.play();
-//music.loop = true;
-//music.volume = 0.1;
-//music.playbackRate = 1;
+let music = new Audio('/music/StartScreen.mp3');
+music.loop = true;
+music.volume = 0.2;
+music.playbackRate = 1;
 
+music.play();
 
 // ------------------------- Funcion para cambiar entre pantallas ----------------------------------- //
 
 const cambiarPantalla = (pantalla) => {
-
+    
     let arrPantallas = ['pantallaInicio', 'pantallaPersonajes', 'pantallaLoading' ,'pantallaCarrera', 'pantallaPodio'];
     arrPantallas = arrPantallas.filter(val => !pantalla.includes(val));
     document.getElementById(pantalla).style.display ='flex';
-        for (let _pantalla of arrPantallas){
-            document.getElementById(_pantalla).style.display = 'none';
-        } 
+    for (let _pantalla of arrPantallas){
+        document.getElementById(_pantalla).style.display = 'none';
+    } 
+    
+    if (pantalla === 'pantallaInicio') {
+            music.pause();
+            music = new Audio('/music/StartScreen.mp3');
+            music.play();
+            music.loop = true;
+            music.volume = 0.2;
+            music.playbackRate = 1;
+    }
+    if (pantalla === 'pantallaPersonajes') {
+        music.pause();
+        music = new Audio('/music/MainMenu.mp3');
+        music.play();
+        music.loop = true;
+        music.volume = 0.2;
+        music.playbackRate = 1;
+    }
+    if (pantalla === 'pantallaInicio') {
+        music.pause();
+        music = new Audio('/music/StartScreen.mp3');
+        music.play();
+        music.loop = true;
+        music.volume = 0.2;
+        music.playbackRate = 1;
+    }
+    if (pantalla === 'pantallaLoading') {
+        music.pause();
+        music = new Audio('/music/MountWario.mp3');
+        music.play();
+        music.loop = true;
+        music.volume = 0.2;
+        music.playbackRate = 1;
+    }
+    if (pantalla === 'pantallaCarrera') {
+        music.pause();
+        music = new Audio('/music/MarioKartStadium.mp3');
+        music.play();
+        music.loop = true;
+        music.volume = 0.2;
+        music.playbackRate = 1;
+    }
+    if (pantalla === 'pantallaPodio') {
+        music.pause();
+        music = new Audio('/music/GoodResult.mp3');
+        music.play();
+        music.loop = true;
+        music.volume = 0.2;
+        music.playbackRate = 1;
+    }
+
         printarSeleccionPersonaje(allPlayers);
 };
 
